301e1e335361a03c23f5944026616af2ff1f73b7
vkopio <ville.kopio@gmail.com>
2019-07-02 11:20:46 +0300

    Added initial tests

---
+++ tests/component/test_synchronizer.py
@@ -0,0 +1,26 @@
+import unittest
+from src.synchronizer import synchronize_group
+from src.directory_api import members
+
+
+def list_all_mock(arg):
+    return []
+
+
+def insert_and_remove_in_batch_mock(*args):
+    return True
+
+
+members.list_all = list_all_mock
+members.insert_and_remove_in_batch = insert_and_remove_in_batch_mock
+
+
+class TestSynchronizer(unittest.TestCase):
+
+    def test_synchronizer(self):
+        self.assertEqual(members.list_all(''), [])
+        self.assertTrue(members.insert_and_remove_in_batch('', [], []))
+
+
+if __name__ == '__main__':
+    unittest.main()
