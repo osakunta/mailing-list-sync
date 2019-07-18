3b6b0af545ed36e2ad07c31cd54a3cbdd13cb829
vkopio <ville.kopio@gmail.com>
2019-07-18 11:46:12 +0300

    Updated connection_builder to handle GCE default credentials with domain-wide delegation

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ ENV = os.environ.get('ENV', 'development')
 GOOGLE_API = {
     'service_account_file': os.environ.get('SERVICE_ACCOUNT_FILE', None),
     'delegated_user': os.environ.get('DELEGATED_USER', None),
+    'toke_uri': 'https://accounts.google.com/o/oauth2/token',
     'directory_scopes': ['https://www.googleapis.com/auth/admin.directory.group'],
     'sheets_scopes': ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 }
--- src/googleintegration/connection_builder.py
+++ src/googleintegration/connection_builder.py
@@ -1,15 +1,44 @@
 from googleapiclient.discovery import build
-from google.auth import default
+from google.auth import default, iam
+from google.auth.transport import requests
+from google.oauth2 import service_account
 from src.config import GOOGLE_API, ENV
 
 
-def connect_to_api(api_name, api_version, scopes=None, delegation=False):
+def update_credentials(credentials, subject, scopes):
+    try:
+        updated_credentials = credentials.with_subject(subject).with_scopes(scopes)
+    except AttributeError:
+        # AttributeError implies that we are using GCE credentials so we cannot
+        # use with_subject and with_scopes. Following link has more details:
+        # https://github.com/GoogleCloudPlatform/professional-services/tree/master/examples/gce-to-adminsdk
+        request = requests.Request()
+        credentials.refresh(request)
+
+        signer = iam.Signer(
+            request,
+            credentials,
+            credentials.service_account_email
+        )
+
+        updated_credentials = service_account.Credentials(
+            signer,
+            credentials.service_account_email,
+            GOOGLE_API['token_uri'],
+            scopes=scopes,
+            subject=subject
+        )
+    except Exception:
+        raise
+
+    return updated_credentials
+
+
+def connect_to_api(api_name, api_version, scopes=None, subject=None):
     if ENV == 'test':
         return
 
-    credentials, _ = default(scopes=scopes)
-
-    if delegation:
-        credentials = credentials.with_subject(GOOGLE_API['delegated_user'])
+    credentials, _ = default()
+    credentials = update_credentials(credentials, subject, scopes)
 
     return build(api_name, api_version, credentials=credentials, cache_discovery=False)
